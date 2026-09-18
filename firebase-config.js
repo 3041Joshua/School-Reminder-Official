a265b0226e61ecdede623849268cdd360e6ac38b
3041Joshua <jsprogament@gmail.com>
2026-09-18 03:49:38 -0600

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Configuración Firebase de School Reminder Web.
+// Estos valores son identificadores del proyecto y pueden estar en el frontend.
+window.SCHOOL_REMINDER_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBRaE_UnBRZlzm24h4-dyO5YuE-nFFSXJw",
+  authDomain: "schoolreminder-5f25b.firebaseapp.com",
+  projectId: "schoolreminder-5f25b",
+  storageBucket: "schoolreminder-5f25b.firebasestorage.app",
+  messagingSenderId: "953831792749",
+  appId: "1:953831792749:web:fcfb1d2aa9d2ece933df64",
+  measurementId: "G-LL464WHY0L"
+};
